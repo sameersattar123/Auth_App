04de164635f02dcb64a012af91348018797118ce
Sameer <sameersattar1111@gmail.com>
2024-04-14 16:39:17 +0500

    sign up functionality complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Profile from "./Pages/Profile";
+import About from "./Pages/About";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-dotenv.config({path : "./.env"});
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
+dotenv.config({ path: "./.env" });
 
-mongoose 
+mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
-  .catch((err) => { 
+  .catch((err) => {
     console.log(err);
   });
 
-
 const app = express();
+app.use(cors()); 
 
 app.use(express.json());
 
-
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     message,
