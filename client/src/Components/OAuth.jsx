3f4add5dfddea0ac0daf39f2367bdf3c44871cd2
Sameer <sameersattar1111@gmail.com>
2024-04-16 02:34:36 +0500

    google

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/slice/userSlice";
 
 const OAuth = () => {
-    const handleGoogleClick = async () => {
-        try {
-                const res = await fetch(`http://localhost:3000/auth/google`)
-                
-        } catch (error) {
-            console.log(error)
-        }
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      const res = await fetch(`http://localhost:3000/auth/google`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      console.log("could not login with google", error);
     }
+  };
   return (
-    <div type='button' onClick={handleGoogleClick} className="bg-red-700 p-3 uppercase text-white text-center rounded-lg hover:opacity-95">
+    <div
+      type="button"
+      onClick={handleGoogleClick}
+      className="bg-red-700 p-3 uppercase text-white text-center rounded-lg hover:opacity-95"
+    >
       Continue with Google
     </div>
   );
