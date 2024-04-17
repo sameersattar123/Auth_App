a19ee0fff2cc4dad8b3517f4aa78e63f60c5faab
Sameer <sameersattar1111@gmail.com>
2024-04-18 03:28:22 +0500

    complete updated functionalilty

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,16 +1,40 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
-export const UpdateUser = async (req,res,next) => {
-    try {
+export const UpdateUser = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      return next(errorHandler(401, "You can update only your account!"));
+    }
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
 
-    } catch (error) {
-        next(error)
-    }
-}
-export const deleteUser = async (req,res,next) => {
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-}
+    const updatedUser = await User.findByIdAndUpdate(
+      req,
+      params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteUser = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { UpdateUser, deleteUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/update/:id", UpdateUser);
-router.post("/delete/:id", deleteUser);
+router.post("/update/:id", verifyToken ,  UpdateUser);
+router.post("/delete/:id", verifyToken , deleteUser);
 
 export default router;
