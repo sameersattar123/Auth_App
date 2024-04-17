bfc62706f99cbb197794900fda0f6085abed2f22
Sameer <sameersattar1111@gmail.com>
2024-04-18 02:44:14 +0500

    new routes and controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js"
+
+export const UpdateUser = async (req,res,next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteUser = async (req,res,next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  signin,
-  signup,
-  google,
-  signout,
-} from "../controllers/auth.controller.js";
+import { UpdateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/signin", signin);
-router.post("/signup", signup);
-router.post("/signout", signout);
-router.post("/google", google);
+router.post("/update/:id", UpdateUser);
+router.post("/delete/:id", deleteUser);
 
 export default router;
